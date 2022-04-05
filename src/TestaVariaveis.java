b2b9499a28768017484e3b32cf2545964f2db449
Yuri Reis <yuri.reis@itsstecnologia.com.br>
2022-04-05 20:46:23 -0300

    Brincando com variáveis

---
+++ src/TestaVariaveis.java
@@ -0,0 +1,18 @@
+public class TestaVariaveis {
+    public static void main(String[] args) {
+
+        // Variável do tipo inteiro
+        int idade = 26;
+        System.out.println(idade);
+
+        // Variável do tipo double
+        double salario = 1250.50;
+        System.out.println(salario);
+
+        // Conversão de variáveis
+        double meuSalario = 2480.75;
+        int meuSalarioInteiro = (int) meuSalario;
+        System.out.println(meuSalario);
+        System.out.println(meuSalarioInteiro);
+    }
+}
